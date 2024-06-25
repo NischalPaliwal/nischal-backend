9502d115117da12d5eb798ccc0003cdc6e84cba5
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-06-25 13:35:43 +0000

    work

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,1 +1,10 @@
-const express = require("express");
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(port);
+// create a todo app that lets users store todos on the server
