b07c10601479b9ea7153c851068d644e4a00820a
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-06-26 10:01:53 +0000

    get

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const port = 3000;
+const port = 4000;
 const app = express();
 
-// app.get('/', function(req, res) {
-// res.send("Hello World!");
-// });
-
-app.get("/route-handler", function(req, res) {
-    res.json({
-      name: "Nischal",
-      age: 20
-        });
+app.get('/', function(req, res) {
+res.send('<b><i>Hello Everyone</i></b>');
 });
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
-
+app.listen(port);
 // create a todo app that lets users store todos on the server
