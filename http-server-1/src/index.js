c6459f964f22ffebe55f1ef56084bcea391de5df
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-06-28 06:02:45 +0000

    edit

---
+++ http-server-1/src/index.js
@@ -0,0 +1,15 @@
+
+
+const express = require("express");
+const app = express();
+const port = 2000;
+
+app.get("/", (request, response) => {
+    response.json({
+        msg: "Hi there from JavaScript"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Running on port value ${port}.`);
+})
